2b94cccec9ab3cdf96e54c019bc78b5081155fda
KLB <karl-ludwig.besser@tu-dresden.de>
2018-10-29 11:47:38 -0700

    Add merging of pgfplot data with MC leakage

--- calc_leakage_mc.py
+++ calc_leakage_mc.py
@@ -1,13 +1,28 @@
+import ast
+
 import numpy as np
+import pandas as pd
 
 def main():
+    data = pd.read_csv("lwc-config1-t5.dat", sep='\t')
     file_full = "results-config1-full.dat"
     file_cond = "results-config1-conditional.dat"
     entr_z = parse_file(file_full)
     entr_zm = parse_file(file_cond)
+    leak = {'wB': [], 'wE': [], 'LeakMC': []}
     for combination, entr in entr_z.items():
         _entr_cond = entr_zm[combination]
+        _leak = (entr-_entr_cond)/np.log(2)
+        #leak[combination] = _leak
         print("{}:\t{}".format(combination, (entr-_entr_cond)/np.log(2)))
+        wb, we = ast.literal_eval(combination)
+        leak['wB'].append(wb)
+        leak['wE'].append(we)
+        leak['LeakMC'].append(_leak)
+    data_new = pd.DataFrame(leak)
+    data_new = pd.merge(data, data_new)
+    print(data_new)
+    data_new.to_csv('lwc-config1-t5-mc.dat', sep='\t', index=False)
 
 def parse_file(filename):
     with open(filename) as infile:
