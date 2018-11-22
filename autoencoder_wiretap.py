3f26f2346f6f814c2b64fa1a55260185e546b134
KLB <karl-ludwig.besser@tu-dresden.de>
2018-11-22 17:39:28 +0100

    Add checkpoint model saving to training

--- autoencoder_wiretap.py
+++ autoencoder_wiretap.py
@@ -7,6 +7,8 @@ from sklearn.metrics import hamming_loss
 from keras import models
 from keras import layers
 from keras import optimizers
+from keras import losses
+from keras import callbacks
 from keras import backend as K
 
 from digcommpy import messages, metrics
@@ -70,6 +72,23 @@ def loss_leakage_gauss_mix(y_true, y_pred, k, r, dim, noise_pow=.5):
     #return K.mean(entr_z - entr_zm, axis=-1)/(np.log(2)*k)
     return K.square(K.mean(entr_z - entr_zm, axis=-1)/(np.log(2)*k))
 
+def loss_leakage_upper(y_true, y_pred, k, r, dim, noise_pow=.5):
+    batch_size = int(2**(r+k))
+    leak_upper_bound = _leak_new_upper_bound(y_pred, noise_pow, batch_size, r, 1.)
+    #return K.square(leak_upper_bound/(np.log(2)*k))
+    return leak_upper_bound/(np.log(2)*k)
+
+def _leak_new_upper_bound(mu, noise_power, batch_size, r, eps):
+    weight = 1./batch_size
+    p_i = K.repeat_elements(mu, batch_size, axis=0)
+    p_j = K.tile(mu, (batch_size, 1))
+    kl = 1./(2*noise_power)*K.batch_dot(p_i-p_j, p_i-p_j, axes=1)
+    kl = K.reshape(kl, (batch_size, batch_size, -1))
+    log_inner = np.log(weight) + K.logsumexp(-kl, axis=1, keepdims=True)
+    outer_sum = K.sum(weight*log_inner, axis=0)
+    mi_upper = -outer_sum - r + eps
+    return mi_upper
+
 def loss_leakage_with_gap(y_true, y_pred, k, r, dim, noise_pow=.5):
     split_len = int(2**r)
     batch_size = int(2**(r+k))
@@ -103,6 +122,16 @@ def _leak_lower_bound(y_pred, sigma, batch_size, dim, split_len, k):
     entr_zm = K.mean(entr_zm, axis=0)
     return K.relu(K.mean(entr_z - entr_zm, axis=-1))/(np.log(2)*k)
     #return K.square(K.mean(entr_z - entr_zm, axis=-1)/(np.log(2)*k))
+
+def loss_log_mse(y_true, y_pred, weight=1.):
+    return -K.log(weight - losses.mse(y_true, y_pred))
+
+def loss_log_leak(y_true, y_pred, k, r, dim, noise_pow=.5, weight=1):
+    split_len = int(2**r)
+    batch_size = int(2**(k+r))
+    sigma = noise_pow * np.eye(dim)
+    leak_upper_bound = _leak_upper_bound(y_pred, sigma, batch_size, dim, split_len, k)
+    return -K.log(weight - leak_upper_bound/(np.log(2)*k))
 
 def loss_gauss_mix_entropy(y_true, y_pred, batch_size, dim, noise_pow=.5, k=1):
     sigma = noise_pow * np.eye(dim)
@@ -196,9 +225,12 @@ def create_model(code_length:int =16, info_length: int =4, activation='relu',
                                     name='output_bob')(layer_list_decoder[-1])
     model = models.Model(inputs=[main_input, random_input],
                          outputs=[output_layer_bob, layer_list_enc[-1]])
-    model.compile(optimizer, loss_weights=loss_weights,
+    model.compile(optimizer,#loss_weights=loss_weights,
                   #loss=['mse', lambda x, y: loss_leakage_gauss_mix(x, y, info_length, random_length, code_length, noise_pow=train_noise['eve'])])
-                  loss=['mse', lambda x, y: loss_leakage_with_gap(x, y, info_length, random_length, code_length, noise_pow=train_noise['eve'])])
+                  #loss=['mse', lambda x, y: loss_leakage_with_gap(x, y, info_length, random_length, code_length, noise_pow=train_noise['eve'])])
+                  loss=['mse', lambda x, y: K.square(loss_leakage_upper(x, y, info_length, random_length, code_length, noise_pow=train_noise['eve']))])
+                  #loss=[lambda x, y: loss_log_mse(x, y, weight=1./loss_weights[0]),
+                  #      lambda x, y: loss_log_leak(x, y, info_length, random_length, code_length, noise_pow=train_noise['eve'], weight=1./loss_weights[1])])
     return model
 
 def _ebn0_to_esn0(snr, rate=1.):
@@ -230,10 +262,12 @@ def loss_weight_sweep(n=16, k=4, train_snr={'bob': 2., 'eve': -5.}, test_snr=0.,
                                       binary=True)
     test_set = [test_info, test_rnd]
     target_eve = np.zeros((len(info_train), n))
-    _weights = np.linspace(0.5, .01, 5)
+    _weights = np.linspace(0.5, .01, 3)
+    #_weights = np.linspace(0.7, 0.2, 8)
     #_weights = np.logspace(np.log10(.25), -4, 20)
     loss_weights = [[1.-k, k] for k in _weights]
     loss_weights.append([1, 0])
+    #loss_weights = [[0.8675, .1325]]
     results_file = 'lwc-B{bob}E{eve}-T{0}-n{1}-k{2}-r{3}.dat'.format(
                     test_snr, n, k, random_length, **train_snr)
     results_file = os.path.join(dirname, results_file)
@@ -242,15 +276,20 @@ def loss_weight_sweep(n=16, k=4, train_snr={'bob': 2., 'eve': -5.}, test_snr=0.,
     for combination in loss_weights:
         print("Loss weight combination: {}".format(combination))
         optimizer = optimizers.Adam.from_config(optimizer_config)
+        #optimizer = optimizers.Adadelta.from_config(optimizer_config)
         model = create_model(n, k, symmetrical=False, loss_weights=combination,
                              train_snr=train_snr, activation='sigmoid',
                              random_length=random_length, nodes_enc=nodes_enc,
                              nodes_dec=nodes_dec, test_snr=test_snr,
                              optimizer=optimizer)
         #print("Start training...")
+        checkpoint_path = "checkpoint-{}-{{epoch:d}}.model".format(combination)
+        checkpoint_path = os.path.join(dirname, checkpoint_path)
+        checkpoint = callbacks.ModelCheckpoint(checkpoint_path, 'loss', save_weights_only=True,
+                                               period=5000)
         history = model.fit([info_train, rnd_train], [info_train, target_eve],
-                            epochs=10000, verbose=0, shuffle=False, 
-                            batch_size=len(info_train))
+                            epochs=500000, verbose=0, shuffle=False, 
+                            batch_size=len(info_train), callbacks=[checkpoint])
         print("Finished training...")
         pred = model.predict(test_set)[0] # Noise is added during testing
         pred_bit = np.round(pred)
@@ -290,7 +329,7 @@ def _save_codebook(model, info_length, random_length, combination, dirname='.'):
     results_file = "codewords-{}.dat".format(combination)
     results_file = os.path.join(dirname, results_file)
     with open(results_file, 'w') as outf:
-        outf.write("messsage\trandom\tcodeword\n")
+        outf.write("message\trandom\tcodeword\n")
         for _info, _rand, _cw in zip(info, rand, codewords):
             outf.write("{}\t{}\t{}\n".format(list(_info), list(_rand), list(_cw)))
     return info, rand, codewords
@@ -310,11 +349,13 @@ def calc_wiretap_leakage(info, codewords, noise_var):
 if __name__ == "__main__":
     code_length = 16
     #combinations = (([code_length], []), ([8*code_length, 4*code_length, code_length], [8*code_length, 4*code_length]))
+    combinations = (([8*code_length, 4*code_length, code_length], [8*code_length, 4*code_length]),)
     #combinations = (([code_length], []),) 
-    combinations = (([16*code_length, code_length], []),)
+    #combinations = (([16*code_length, code_length], []),)
     for comb in combinations:
             train_snr = {'bob': 0., 'eve': -5.}
             opt_conf = optimizers.Adam(amsgrad=False).get_config()
+            #opt_conf = optimizers.Adadelta().get_config()
             history, model = loss_weight_sweep(n=code_length, k=4, train_snr=train_snr,
             test_snr=0., random_length=3, test_size=1e4, nodes_enc=comb[0],
             nodes_dec=comb[1], test_snr_eve=-5., optimizer_config=opt_conf,
